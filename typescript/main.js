e4a79d0fa3156f0ac69e93cbed75941a818097b2
jarperw <you@example.com>
2022-11-14 19:49:08 +0100

    ejercicio-2

--- typescript/main.js
+++ typescript/main.js
@@ -1,8 +1,15 @@
 "use strict";
 var _a;
+//varibales globales
 var URL1 = "https://icanhazdadjoke.com/";
+var joke = document.getElementById('joke');
+//EJERCICIO 1
 (_a = document.getElementById("nextJoke")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
     fetch(URL1, { headers: { "Accept": "application/json" } })
         .then(function (resp) { return resp.json(); })
-        .then(function (chiste) { return console.log(chiste.joke); });
+        .then(function (chiste) { return mostrarChiste(chiste.joke); });
 });
+//EJERCICIO 2
+var mostrarChiste = function (chiste) {
+    joke.innerHTML = "\"".concat(chiste, "\"");
+};
--- typescript/main.ts
+++ typescript/main.ts
@@ -1,9 +1,18 @@
 "use strict";
 
+//varibales globales
 const URL1: string = "https://icanhazdadjoke.com/";
+const joke = document.getElementById('joke');
 
+
+//EJERCICIO 1
 document.getElementById("nextJoke")?.addEventListener("click", ()=>{
     fetch(URL1, {headers: {"Accept": "application/json" }})
         .then( resp => resp.json())
-        .then( chiste => console.log(chiste.joke))
+        .then( chiste => mostrarChiste(chiste.joke))
 })
+
+//EJERCICIO 2
+const mostrarChiste = (chiste) => {
+    joke!.innerHTML = `"${chiste}"`;
+}
