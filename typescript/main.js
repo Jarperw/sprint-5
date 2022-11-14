de9a2cab141770d67a2829cf364cb9378bc6c0a7
jarperw <you@example.com>
2022-11-14 21:22:51 +0100

    ejercicio-3

--- typescript/main.js
+++ typescript/main.js
@@ -3,13 +3,51 @@ var _a;
 //varibales globales
 var URL1 = "https://icanhazdadjoke.com/";
 var joke = document.getElementById('joke');
+var displayScore = document.getElementById('score');
+var reportJokes = [];
+var encuesta = false;
+var value;
+// eventos
+document.getElementById('1').onclick = reporte;
+document.getElementById('2').onclick = reporte;
+document.getElementById('3').onclick = reporte;
 //EJERCICIO 1
 (_a = document.getElementById("nextJoke")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
-    fetch(URL1, { headers: { "Accept": "application/json" } })
-        .then(function (resp) { return resp.json(); })
-        .then(function (chiste) { return mostrarChiste(chiste.joke); });
+    if (!encuesta) {
+        fetch(URL1, { headers: { "Accept": "application/json" } })
+            .then(function (resp) { return resp.json(); })
+            .then(function (chiste) {
+            value = chiste.joke;
+            mostrarChiste(chiste.joke);
+        });
+    }
 });
 //EJERCICIO 2
 var mostrarChiste = function (chiste) {
     joke.innerHTML = "\"".concat(chiste, "\"");
+    // displayScore.setAttribute ('class','d-block');
+    mostrarEncuesta('d-block');
+    valorEncuesta();
 };
+//EJERCICIO 3
+function reporte() {
+    if (encuesta) {
+        var puntos = Number(this.getAttribute('id'));
+        var fecha = new Date();
+        reportJokes.push({
+            joke: value,
+            score: puntos,
+            date: fecha.toISOString()
+        });
+        console.log('reporte:', reportJokes);
+        valorEncuesta();
+        borrar();
+    }
+}
+var borrar = function () {
+    joke.innerHTML = "";
+    // displayScore.setAttribute ('class','d-none');
+    mostrarEncuesta('d-none');
+};
+var mostrarEncuesta = function (valor) { return displayScore.setAttribute('class', valor); };
+var valorEncuesta = function () { return encuesta = !encuesta; };
--- typescript/main.ts
+++ typescript/main.ts
@@ -2,17 +2,62 @@
 
 //varibales globales
 const URL1: string = "https://icanhazdadjoke.com/";
-const joke = document.getElementById('joke');
+const joke: any = document.getElementById('joke');
+const displayScore: any = document.getElementById('score');
+const reportJokes: any[] = [];
+let encuesta: boolean = false;
+let value: string ;
+
+// eventos
+document.getElementById('1')!.onclick = reporte;
+document.getElementById('2')!.onclick = reporte;
+document.getElementById('3')!.onclick = reporte;
 
 
 //EJERCICIO 1
 document.getElementById("nextJoke")?.addEventListener("click", ()=>{
-    fetch(URL1, {headers: {"Accept": "application/json" }})
+    if(!encuesta){
+        fetch(URL1, {headers: {"Accept": "application/json" }})
         .then( resp => resp.json())
-        .then( chiste => mostrarChiste(chiste.joke))
+        .then( chiste => {
+            value = chiste.joke;
+            mostrarChiste(chiste.joke)
+        })
+    }
 })
 
 //EJERCICIO 2
-const mostrarChiste = (chiste) => {
+const mostrarChiste = (chiste: string) => {
     joke!.innerHTML = `"${chiste}"`;
+    // displayScore.setAttribute ('class','d-block');
+    mostrarEncuesta('d-block');
+    valorEncuesta();
 }
+
+//EJERCICIO 3
+function reporte(){
+    if(encuesta){
+        const puntos:number = Number(this.getAttribute('id'));
+        const fecha: any = new Date();    
+        
+        reportJokes.push({
+            joke: value,
+            score: puntos,
+            date: fecha.toISOString()
+        }); 
+            
+        console.log( 'reporte:', reportJokes);
+        valorEncuesta();
+        borrar();
+    }
+}
+
+const borrar = () =>{
+    joke.innerHTML = "";
+    // displayScore.setAttribute ('class','d-none');
+    mostrarEncuesta('d-none');
+}
+
+const mostrarEncuesta = (valor:string) => displayScore.setAttribute('class',valor);
+
+const valorEncuesta = () => encuesta = !encuesta;
